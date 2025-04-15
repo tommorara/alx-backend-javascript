ad67fab96a9f02be81cd4433e3462498ccabb5ac
Tom Morara <morara1920@gmail.com>
2025-04-15 04:29:55 +0300

    revised

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
 export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-    return [task, task2];
-  } else {
-    const task = false;
-    const task2 = true;
-    return [task, task2];
+    // These variables are block scoped and do not affect the outer ones.
+    const innerTask = true;
+    const innerTask2 = false;
   }
+
+  return [task, task2];
 }
 

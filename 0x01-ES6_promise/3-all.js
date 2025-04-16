ad7caa42923af4555431104c4ddd9c448e76327a
Tom Morara <morara1920@gmail.com>
2025-04-16 22:00:45 +0300

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils.js';
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoRes, userRes]) => {
+      console.log(photoRes.body, userRes.firstName, userRes.lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}

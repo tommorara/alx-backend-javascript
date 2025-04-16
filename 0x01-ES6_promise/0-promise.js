5de7037fb1f5b2887393fe6cd64a60cc5259af81
Tom Morara <morara1920@gmail.com>
2025-04-16 21:53:22 +0300

    revised

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,14 +2,3 @@ export default function getResponseFromAPI() {
   return Promise.resolve();
 }
 
-/* 1-promise.js */
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
-}
-

6e069684a9c18b63b420d0457a5615d2da9adc83
Tom Morara <morara1920@gmail.com>
2025-04-16 22:24:31 +0300

    revised

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,17 @@
 import { uploadPhoto, createUser } from './utils.js';
+
+/**
+ * Uploads a photo and creates a user concurrently.
+ * Returns an object with the photo and user data.
+ * If either operation fails, returns an object with null values.
+ * @returns {Promise<{photo: Object|null, user: Object|null}>}
+ */
 export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
     return { photo, user };
-  } catch {
+  } catch (error) {
     return { photo: null, user: null };
   }
 }

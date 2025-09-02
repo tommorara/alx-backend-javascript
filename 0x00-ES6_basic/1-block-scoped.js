4cc13c07d64ae6cc215083bd119998241a0cdef9
Tom Morara <morara1920@gmail.com>
2025-09-02 13:53:03 +0300

    020925

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
-export default function taskBlock (trueOrFalse) {
-  const task = false;
-  const task2 = true;
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
 }
+
